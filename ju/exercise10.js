cab2baca6e1efd8b5994d267b2f6dc500efaf9a5
hanad <hanadaadn20@gmail.com>
2025-12-20 13:31:41 +0300

    first commit

---
+++ ju/exercise10.js
@@ -0,0 +1,15 @@
+let libarary = [
+    {
+        title: "power",
+        author: "hanad",
+        year: "1987",
+    
+    },
+    {
+        title: "lift",
+        author: "john",
+        year: "1991",
+    }
+]
+console.log(libarary[0].title)
+console.log(libarary[1].title)
