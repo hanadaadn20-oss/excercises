bc03e795ced8202f37b5bbfa87dd4fe7eb3ad93f
hanad <hanadaadn20@gmail.com>
2025-12-22 14:00:43 +0300

    new commit

---
+++ je/jssss.js
@@ -0,0 +1,14 @@
+let tempreature = 27
+if(tempreature < 0){
+    console.log("very cold")
+}
+else if(tempreature <= 15){
+    console.log("cold")
+}
+else if(tempreature <= 25){
+    console.log("warm")
+}
+
+else if(tempreature > 25){
+    console.log("hot")
+}
