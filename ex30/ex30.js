d58e3d178f7c2cd226284c96934aeed62974ac19
hanad <hanadaadn20@gmail.com>
2026-01-11 13:50:04 +0300

    new commit

--- ex30/ex30.js
+++ ex30/ex30.js
@@ -1,20 +1,2 @@
-function oprate (a,b,callback){
-    return callback(a,b)
-}
-function add (a,b){
-    return a+b;
-}
-function subtract (a,b){
-    return a-b;
-}
-function multipple (a,b){
-    return a*b;
-}
-function divide (a,b){
-    return a/b;
-}
-
-console.log("add:",oprate(2,3,add))
-console.log("subtract:",oprate(2,9,subtract))
-console.log("multipple:",oprate(2,6,multipple))
-console.log("divide:",oprate(2,3,divide))
+const print = document.querySelector('.ha')
+console.log(print)
