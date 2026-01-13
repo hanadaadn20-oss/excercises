0a6f62d392db94afaaed0bf2377107f068f34eeb
hanad <hanadaadn20@gmail.com>
2026-01-13 12:26:51 +0300

    new commit

---
+++ ex33/ex33.js
@@ -0,0 +1,17 @@
+function black(){
+    document.body.style.backgroundColor='black'
+    document.body.style.color='white'
+}
+function light(){
+    const why = prompt("put you background color")
+    document.body.style.backgroundColor=
+    document.body.style.color='black'
+    const width = prompt("put the width")
+        document.body.style.width='width'
+  const height = prompt("put the height")
+   document.body.style.height='height'
+   const padding= prompt("enter the padding")
+   document.body.style.padding='padding'
+   const what=prompt('enter the border raduis')
+   document.body.style.border='what'
+}
